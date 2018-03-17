cfeae645ad3e536dace5b701e1b341d88acc9732
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-03-17 06:04:34 -0500

    adding questions and JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,50 +1,126 @@
 $(document).ready(function() {
-    $('#play').remove();
-    var count = 10;
+    $('#play').on('click',function(){
+        game.start();
+       
+      })
+      $(document).on('click','#end',function(){
+        game.over();
+      })
+
     var questions =[{
         question: "How many official planets are in the solar system?", 
-        choices: ["7", "8", "9"],
-        correctAnswer: 1,
+        answers: ["7", "8", "9"],
+        correctAnswer: [1],
     } , {
         question: "When two full moons are in the same month, the second is called a blue moon. How often do blue moons occur?", 
-        choices: ["1 1/2 to 2 years", "2 1/2 to 3 years", "4 1/2 to 5 years"],
-        correctAnswer: 1,
+        answers: ["1 1/2 to 2 years", "2 1/2 to 3 years", "4 1/2 to 5 years"],
+        correctAnswer: [1],
     } , {
         question: "What color is the outermost arc on a rainbow?", 
-        choices: ["Violet", "Green", "Red"],
-        correctAnswer: 2,
+        answers: ["Violet", "Green", "Red"],
+        correctAnswer: [2],
     } , {
         question: "What is sometimes called the Evening Star and sometimes called the Morning Star?", 
-        choices: ["Venus", "Mars", "Bellatrix"],
-        correctAnswer: 0,
+        answers: ["Venus", "Mars", "Bellatrix"],
+        correctAnswer: [0],
     } , {
         question: "Caroline Herschel discovered eight comets, her brother William discovered which planet?", 
-        choices: ["Jupiter", "Mercury", "Uranus"],
-        correctAnswer: 2,
+        answers: ["Jupiter", "Mercury", "Uranus"],
+        correctAnswer: [2],
     } , {
         question: "Miranda, Bianca, Ophelia, Portia, and Cordelia are all moons of Uranus, they are also characters in which man's plays?", 
-        choices: ["Samuel Beckett", "William Shakespeare", "George Bernard Shaw"],
-        correctAnswer: 1,    
+        answers: ["Samuel Beckett", "William Shakespeare", "George Bernard Shaw"],
+        correctAnswer: [1],    
     } , {
         question: "The four moons orbiting Jupiter are Io, Ganymede, Callisto, and Europa. Who discovered them?", 
-        choices: ["Gallileo Galilei", "Johannes Kepler", "Nicolaus Copernicus"],
-        correctAnswer: 1,  
+        answers: ["Gallileo Galilei", "Johannes Kepler", "Nicolaus Copernicus"],
+        correctAnswer: [1],  
     }]
 
-    var triviaGame = {
-        timer: 100,
-        correct: 0,
-        incorrect: 0,
+    var game = {
+     correct: 0,
+     incorrect: 0,
+     timer: 100,
+     counter: function(){
+     game.seconds--;
+     $('#seconds').html(game.seconds);
+     if(game.seconds<=0){
+     console.log("time!");
+     game.over();
+   }
+   },
+     
+   start: function(){
+            seconds = setInterval(game.counter,1000);
+            $('#interiorContainer').prepend('<h2>Remaining Time: <span id="seconds">100</span> Seconds</h2>')
+             $('#play').remove();
+              for(var i=0; i<questions.length; i++){
+               $('#interiorContainer').append('<h2>'+questions[i].question+'</h2>');
+            for(var j=0; j<questions[i].answers.length;j++){
+            $('#interiorContainer').append("<input type='radio' name='question-'"+i+"'value=''"+questions[i].answers[j]+"'>"+questions[i].answers[j])
+        }
       }
-var counter = setInterval(timer, 1000);
-
-function timer()
-{
-  count = count - 1;
-  if (count <= 0)
-  {
-     triviaGame.done();
-  }
-  $("#timer").html(game.timer);
+       $('#interiorContainer').append('<button id="end">Done</button>');
+     },
+       over: function(){
+         $.each($('input[name="question-1]":selected'),function(){
+           if($(this).val()==questions[1].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+          $.each($('input[name="question-2]":selected'),function(){
+           if($(this).val()==questions[2].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+       $.each($('input[name="question-3]":selected'),function(){
+           if($(this).val()==questions[3].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+        $.each($('input[name="question-0]":selected'),function(){
+           if($(this).val()==questions[0].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+         $.each($('input[name="question-4]":selected'),function(){
+           if($(this).val()==questions[4].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+        $.each($('input[name="question-5]":selected'),function(){
+           if($(this).val()==questions[5].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         })
+        $.each($('input[name="question-6]":selected'),function(){
+           if($(this).val()==questions[6].correctAnswer){
+             game.correct++;
+           } else {
+             game.incorrect++;
+           }
+         });
+    this.results();
+       },
+    results: function(){
+      clearInterval(timer);
+      $('#interiorContainer').html("<h2>Congratulations!</h2>");
+      $('#interiorContainer').append("<h3>You got "+this.correct+"right!</h3>")
+      $('#interiorContainer').append("<h3>You got "+this.incorrect+"wrong.<h3>")
+      $('#interiorContainer').append("<h3> And you didn't answer "+(questions.length-(this.incorrect+this.correct))+"<h3>")
+    }
 }
+
 });
